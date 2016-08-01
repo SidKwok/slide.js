a4619a12ddd6f640eeac0dad7186cf06917557aa
SidKwok <oceankwok@hotmail.com>
2016-08-01 12:20:20 +0800

    add DEFAULT and init what i need

--- src/js/slide.js
+++ src/js/slide.js
@@ -1,3 +1,31 @@
-(() => {
+/**
+ * small, simple, jQuery-free Slides
+ * @author SidKwok
+ */
 
-})();
+(function() {
+    const DEFAULT = {
+        imgs: [],
+        width: 500,
+        height: 300,
+        animation: {
+            open: false,
+            delay: 2000
+        }
+    }
+
+    class Slide {
+        constructor(opt) {
+            this.opt = opt;
+            console.log(this.opt)
+        }
+        test() {
+            console.log(';yo');
+        }
+    }
+
+    /**
+     * expose Slide
+     */
+    window.Slide = Slide;
+})()
